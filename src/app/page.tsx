8cd7aeb3c4088efe5ecdcbe39c77ea4ee2d75230
Cole Cummins <colejcummins@github.com>
2024-08-20 17:48:37 -0700

    stash new background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,53 @@ import React from 'react';
 
 import { Console } from '@/components/Console';
 import { ThemeProvider } from '@/components/ThemeProvider';
-// import { Background } from '@/components/Background';
+import { Background } from '@/components/Background';
 
 export default function Home() {
+  return (
+    <ThemeProvider>
+      <div className="relative flex justify-center items-center w-screen h-screen bg-transparent">
+        <Background />
+        <div className="flex flex-col rounded-lg w-4/5 bg-slate-100 dark:bg-slate-950 border border-slate-300 dark:border-slate-700">
+          <div className="flex gap-2 justify-start p-2 border-b border-slate-200 dark:border-slate-800">
+            {Array(3)
+              .fill(0)
+              .map((i) => (
+                <div key={i} className="rounded-full h-4 w-4 border border-slate-200 dark:border-slate-800" />
+              ))}
+          </div>
+          <Console />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+/**
+ *
+export default function Home() {
+  return (
+    <ThemeProvider>
+      <div className="relative flex justify-center items-center w-screen h-screen bg-transparent">
+        <Background />
+        <div className="flex flex-col rounded-lg w-4/5 bg-slate-100 dark:bg-slate-950 border border-slate-300 dark:border-slate-700">
+          <div className="flex gap-2 justify-start p-2 border-b border-slate-200 dark:border-slate-800">
+            {Array(3)
+              .fill(0)
+              .map((i) => (
+                <div key={i} className="rounded-full h-4 w-4 border border-slate-200 dark:border-slate-800" />
+              ))}
+          </div>
+          <Console />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+ */
+
+/**
+ * export default function Home() {
   return (
     <ThemeProvider>
       <div className="flex justify-center items-center w-screen h-screen bg-white dark:bg-black">
@@ -26,10 +70,5 @@ export default function Home() {
     </ThemeProvider>
   );
 }
-
-/**
- *
-      <div className="flex flex-1 items-center justify-center">
-      <Background />
-    </div>
  */
+
---
+++ src/components/Background.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Background = () => {
+  return (
+    <div className="absolute w-screen h-screen -z-10">
+      <iframe
+        loading='lazy'
+        src='https://meshbg-1xcve0hlq-chasedavis.vercel.app/embed/0.5627756749092943'
+      />
+    </div>
+  )
+};
