474841c9c5ff4f03218e0284ed8c58c622882667
Cole Cummins <colejcummins@github.com>
2024-08-20 17:48:54 -0700

    linting

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 export const Background = () => {
   return (
     <div className="absolute w-screen h-screen -z-10">
-      <iframe
-        loading='lazy'
-        src='https://meshbg-1xcve0hlq-chasedavis.vercel.app/embed/0.5627756749092943'
-      />
+      <iframe loading="lazy" src="https://meshbg-1xcve0hlq-chasedavis.vercel.app/embed/0.5627756749092943" />
     </div>
-  )
+  );
 };
